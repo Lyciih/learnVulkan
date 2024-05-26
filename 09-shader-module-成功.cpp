77132255858e200d097cabeca4788526c14ae03a
Lyciih <gnehciyil@gmail.com>
2024-05-26 12:46:26 +0800

    fixed functions

--- 09-shader-module-成功.cpp
+++ 09-shader-module-成功.cpp
@@ -96,8 +96,6 @@ private:
 
 	std::vector<VkImageView> swapChainImageViews;
 
-	VkPipelineLayout pipelineLayout;
-
 
 	
 	void initWindow(){
@@ -148,8 +146,6 @@ private:
 	
 
 	void cleanup(){
-		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
-
 		for(auto imageView : swapChainImageViews){
 			vkDestroyImageView(device, imageView, nullptr);
 		}
@@ -372,7 +368,6 @@ private:
 		std::cout << "shaderFloat64:\t" << deviceFeatures.shaderFloat64 << std::endl;
 		std::cout << "shaderInt64:\t" << deviceFeatures.shaderInt64 << std::endl;
 		std::cout << "geometryShader:\t" << deviceFeatures.geometryShader << std::endl;
-		std::cout << "wideLines:\t" << deviceFeatures.wideLines << std::endl;
 
 		QueueFamilyIndices indices = findQueueFamilies(device);
 
@@ -580,7 +575,6 @@ private:
 			queueCreateInfos.push_back(queueCreateInfo);
 		}
 
-		// 填入想啟用的邏輯設備特性
 		VkPhysicalDeviceFeatures deviceFeatures{};
 
 		VkDeviceCreateInfo createInfo{};
@@ -748,111 +742,6 @@ private:
 		VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
 
 
-		// dynamic state
-		std::vector<VkDynamicState> dynamicStates = {
-			VK_DYNAMIC_STATE_VIEWPORT,
-			VK_DYNAMIC_STATE_SCISSOR
-		};
-
-		VkPipelineDynamicStateCreateInfo dynamicState{};
-		dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
-		dynamicState.dynamicStateCount = static_cast<uint32_t>(dynamicStates.size());
-		dynamicState.pDynamicStates = dynamicStates.data();
-
-		// vertex input
-		VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
-		vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-		vertexInputInfo.vertexBindingDescriptionCount = 0;
-		vertexInputInfo.pVertexBindingDescriptions = nullptr;	// optional
-		vertexInputInfo.vertexAttributeDescriptionCount = 0;
-		vertexInputInfo.pVertexAttributeDescriptions = nullptr;	// optional
-
-		// input assembly
-		VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
-		inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-		inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-		inputAssembly.primitiveRestartEnable = VK_FALSE;
-
-		// viewports and scissors
-		VkViewport viewport{};
-		viewport.x = 0.0f;
-		viewport.y = 0.0f;
-		viewport.width = (float) swapChainExtent.width;
-		viewport.height = (float) swapChainExtent.height;
-		viewport.minDepth = 0.0f;
-		viewport.maxDepth = 1.0f;
-
-		VkRect2D scissor{};
-		scissor.offset = {0, 0};
-		scissor.extent = swapChainExtent;
-
-		VkPipelineViewportStateCreateInfo viewportState{};
-		viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
-		viewportState.viewportCount = 1;
-		viewportState.pViewports = &viewport;
-		viewportState.scissorCount = 1;
-		viewportState.pScissors = &scissor;
-
-		// Rasterizer
-		VkPipelineRasterizationStateCreateInfo rasterizer{};
-		rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
-		rasterizer.depthClampEnable = VK_FALSE;
-		rasterizer.rasterizerDiscardEnable = VK_FALSE;
-		rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
-		rasterizer.lineWidth = 1.0f;
-		rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-		rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
-		rasterizer.depthBiasEnable = VK_FALSE;
-		rasterizer.depthBiasConstantFactor = 0.0f; // optional
-		rasterizer.depthBiasClamp = 0.0f; // optional
-		rasterizer.depthBiasSlopeFactor = 0.0f; // optional
-
-		// Multisampling
-		VkPipelineMultisampleStateCreateInfo multisampling{};
-		multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-		multisampling.sampleShadingEnable = VK_FALSE;
-		multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
-		multisampling.minSampleShading = 1.0f;	//	optional
-		multisampling.pSampleMask = nullptr;	//	optional
-		multisampling.alphaToCoverageEnable = VK_FALSE; //	optional
-		multisampling.alphaToOneEnable = VK_FALSE;	//	optional
-
-		// color blending
-		VkPipelineColorBlendAttachmentState colorBlendAttachment{};
-		colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
-		colorBlendAttachment.blendEnable = VK_FALSE;
-		colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;	// optional
-		colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;	// optional
-		colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;	// optional
-		colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;	// optional
-		colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;	// optional
-		colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;	// optional
-
-		VkPipelineColorBlendStateCreateInfo colorBlending{};
-		colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
-		colorBlending.logicOpEnable = VK_FALSE;
-		colorBlending.logicOp = VK_LOGIC_OP_COPY;	// optional
-		colorBlending.attachmentCount = 1;
-		colorBlending.pAttachments = &colorBlendAttachment;
-		colorBlending.blendConstants[0] = 0.0f;	// optional
-		colorBlending.blendConstants[1] = 0.0f;	// optional
-		colorBlending.blendConstants[2] = 0.0f;	// optional
-		colorBlending.blendConstants[3] = 0.0f;	// optional
-
-		// pipeline layout
-		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
-		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-		pipelineLayoutInfo.setLayoutCount = 0;	// optional
-		pipelineLayoutInfo.pSetLayouts = nullptr;	// optional
-		pipelineLayoutInfo.pushConstantRangeCount = 0;	// optional
-		pipelineLayoutInfo.pPushConstantRanges = nullptr;	// optional
-
-		if(vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
-			throw std::runtime_error("failed to create pipeline layout!");
-		}
-
-
-		// 釋放 shader module
 		vkDestroyShaderModule(device, fragShaderModule, nullptr);
 		vkDestroyShaderModule(device, vertShaderModule, nullptr);
 	}
