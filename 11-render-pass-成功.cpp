112e00576fe4a3f2a7efab7dab50234861c8e5be
Lyciih <gnehciyil@gmail.com>
2024-05-27 06:23:32 +0800

    graphics pipeline complete

--- 11-render-pass-成功.cpp
+++ 11-render-pass-成功.cpp
@@ -98,7 +98,6 @@ private:
 
 	VkRenderPass renderPass;
 	VkPipelineLayout pipelineLayout;
-	VkPipeline graphicsPipeline;
 
 
 
@@ -152,7 +151,6 @@ private:
 	
 
 	void cleanup(){
-		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyRenderPass(device, renderPass, nullptr);
 
@@ -888,32 +886,6 @@ private:
 
 		if(vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
 			throw std::runtime_error("failed to create pipeline layout!");
-		}
-
-
-		// 正式產生pipeline
-		VkGraphicsPipelineCreateInfo pipelineInfo{};
-		pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
-		pipelineInfo.stageCount = 2;
-		pipelineInfo.pStages = shaderStages;
-
-		pipelineInfo.pVertexInputState = &vertexInputInfo;
-		pipelineInfo.pInputAssemblyState = &inputAssembly;
-		pipelineInfo.pViewportState = &viewportState;
-		pipelineInfo.pRasterizationState = &rasterizer;
-		pipelineInfo.pMultisampleState = &multisampling;
-		pipelineInfo.pDepthStencilState = nullptr;	// optional
-		pipelineInfo.pColorBlendState = &colorBlending;
-		pipelineInfo.pDynamicState = &dynamicState;
-
-		pipelineInfo.layout = pipelineLayout;
-		pipelineInfo.renderPass = renderPass;
-		pipelineInfo.subpass = 0;
-		pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;	// optional
-		pipelineInfo.basePipelineIndex = -1;	// optional
-
-		if(vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS){
-			throw std::runtime_error("failed to create graphics pipeline!");
 		}
 
 
