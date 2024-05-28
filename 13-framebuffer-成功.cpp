2f21b377fd0e5eb1cb30cce16fb9a73e3ccff2ca
Lyciih <gnehciyil@gmail.com>
2024-05-28 15:20:26 +0800

    command buffer

--- 13-framebuffer-成功.cpp
+++ 13-framebuffer-成功.cpp
@@ -95,15 +95,12 @@ private:
 	VkExtent2D swapChainExtent;
 
 	std::vector<VkImageView> swapChainImageViews;
-	std::vector<VkFramebuffer> swapChainFramebuffers;
 
 	VkRenderPass renderPass;
 	VkPipelineLayout pipelineLayout;
 	VkPipeline graphicsPipeline;
 
-	VkCommandPool commandPool;
-	VkCommandBuffer commandBuffer;
-
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 
 
 	
@@ -137,8 +134,7 @@ private:
 		createRenderPass();
 		createGraphicsPipeline();
 		createFramebuffers();
-		createCommandPool();
-		createCommandBuffer();
+	
 	}
 
 	
@@ -158,8 +154,6 @@ private:
 	
 
 	void cleanup(){
-		vkDestroyCommandPool(device, commandPool, nullptr);
-
 		for(auto framebuffer : swapChainFramebuffers){
 			vkDestroyFramebuffer(device, framebuffer, nullptr);
 		}
@@ -954,85 +948,6 @@ private:
 			if(vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS){
 				throw std::runtime_error("failed to create framebuffer!");
 			}
-		}
-	}
-
-	// 產生command pool
-	void createCommandPool(){
-		QueueFamilyIndices queueFamilyIndices = findQueueFamilies(physicalDevice);
-
-		VkCommandPoolCreateInfo poolInfo{};
-		poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
-		poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
-		poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value();
-
-		if(vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS){
-			throw std::runtime_error("failed to create command pool!");
-		}
-	}
-
-	//產生command buffer
-	void createCommandBuffer(){
-		VkCommandBufferAllocateInfo allocInfo{};
-		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-		allocInfo.commandPool = commandPool;
-		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-		allocInfo.commandBufferCount = 1;
-
-		if(vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer) != VK_SUCCESS){
-			throw std::runtime_error("failed to allocate command buffers!");
-		}
-	}
-
-	// 錄製命令
-	void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex){
-		
-		VkCommandBufferBeginInfo beginInfo{};
-		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-		beginInfo.flags = 0;	// optional
-		beginInfo.pInheritanceInfo = nullptr;	// optional
-		
-		if(vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS){
-			throw std::runtime_error("failed to begin recording comamnd buffer!");
-		}
-
-
-
-
-		VkRenderPassBeginInfo renderPassInfo{};
-		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-		renderPassInfo.renderPass = renderPass;
-		renderPassInfo.framebuffer = swapChainFramebuffers[imageIndex];
-		renderPassInfo.renderArea.offset = {0, 0};
-		renderPassInfo.renderArea.extent = swapChainExtent;
-		
-		VkClearValue clearColor = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
-		renderPassInfo.clearValueCount = 1;
-		renderPassInfo.pClearValues = &clearColor;
-
-		vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-
-			vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
-			VkViewport viewport{};
-			viewport.x = 0.0f;
-			viewport.y = 0.0f;
-			viewport.width = static_cast<float>(swapChainExtent.width);
-			viewport.height = static_cast<float>(swapChainExtent.height);
-			viewport.minDepth = 0.0f;
-			viewport.maxDepth = 1.0f;
-			vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
-
-			VkRect2D scissor{};
-			scissor.offset = {0, 0};
-			scissor.extent = swapChainExtent;
-			vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
-
-			vkCmdDraw(commandBuffer, 3, 1, 0, 0);
-
-		vkCmdEndRenderPass(commandBuffer);
-
-		if(vkEndCommandBuffer(commandBuffer) != VK_SUCCESS){
-			throw std::runtime_error("failed to record command buffer!");
 		}
 	}
 };
